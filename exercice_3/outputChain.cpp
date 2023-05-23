2b24f6e36f0a6d3cbc8d472ceb5f6224ad1ca01c
Quentin <109299545+ClueXIII@users.noreply.github.com>
2023-05-23 10:35:23 +0200

    outputchainodes a debug

--- exercice_3/outputChain.cpp
+++ exercice_3/outputChain.cpp
@@ -68,5 +68,55 @@ int getpriority (char op) {
 
 
 void outputChain::InfToSuf(string expression) {
-
+    pile p;
+    string op = "";
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '\0') {
+            while (!p.vide()) { // tant que la pile n'est pas vide
+                this->add(
+                        p.depiler()); // on ajoute les opérateurs de la pile à la chaine de sortie jusqu'à ce que la pile soit vide
+            }
+            continue;
+        }
+        noeud *n = new noeud(expression[i]);
+        if (n->type == 'f') {
+            op = expression[i];
+            while (getpriority(expression[i + 1]) == -1 && expression[i + 1] !=
+                                                           '\0') { // tant que le caractére suivant est un opérande différant de vide = c'est un chiffre
+                op += expression[i + 1]; // on concaténe les chiffres pour former le nombre final
+                i++;// on incrémente i pour passer au caractére suivant
+            }
+            n->val = stof(
+                    op); // on convertit la chaine de caractére en float pour passer la nouvelle valeur à la variable val du noeud
+            this->add(
+                    n); //on ajoute directement le chiffre/nombre à la chaine de sortie suivit d'un espace pour séparer les opérandes
+        } else if (!p.vide()) {
+            if (getpriority(expression[i]) ==
+                3) { // si expression[i] (que l'on passe en string) est un ^ soit le seul oprérateur (utilisé ici) avec une associativité à droite en cpp
+                while (getpriority(expression[i]) < getpriority(
+                        p.getHead()->ope)) { // tant que l'opérateur au sommet de la pile est strictement plus prioritaire que celui de l'expression
+                    this->add(p.depiler()); // on ajoute l'opérateur de la pile à la chaine de sortie
+                }
+            } else if (getpriority(expression[i]) ==
+                       0) { // si expression[i] (que l'on passe en string) est une parenthèse ouvrante
+                p.empiler(n); // on empile la parenthèse ouvrante directement
+            } else if (getpriority(expression[i])  == 4) { // si expression[i] (que l'on passe en string) est une parenthèse fermante
+                while (p.getHead()->ope !=
+                       '(') { // tant que l'opérateur au sommet de la pile n'est pas une parenthèse ouvrante
+                    this->add(p.depiler()); // on ajoute l'opérateur de la pile à la chaine de sortie
+                }
+                p.depiler(); // on dépile ensuite la parenthèse ouvrante
+            } else { // pour tout les autres opérateurs qui on une associativité de gauche a droite
+                while (!p.vide() && getpriority(expression[i])  <= getpriority(p.getHead()->ope)) {// tant que l'opérateur au sommet de la pile est strictement plus prioritaire que celui de l'expression
+                    cout << "vide : " << p.vide() << endl;
+                    cout << "val1 : " << getpriority(expression[i])  << endl;
+                    cout << "val2 : " << getpriority(p.getHead()->ope) << endl;
+                    this->add(p.depiler()); // on ajoute l'opérateur de la pile à la chaine de sortie
+                }
+                p.empiler(n); // on empile l'opérateur de l'expression
+            }
+        } else { // si la pile est vide et que c'est un operateur
+            p.empiler(n); // on empile l'opérateur de l'expression
+        }
+    }
 }
